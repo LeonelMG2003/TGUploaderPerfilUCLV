a0ada73aa85f171aecebc07f8d2548ab870bddc0
LeonelMG2003 <89432777+LeonelMG2003@users.noreply.github.com>
2022-05-16 20:13:30 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = '5335956289:AAH1pYRZRp6LkdtE9U_qFpuwO2nNEbw1GzM'
+API_ID = 9514816
+API_HASH = '2f5b35e5872ddbfe1d4a7885f241044c'
+
+
+# DATOS DEL MOODLE
+USUARIO = 'lpvidal'
+PASSWORD = 'tele123#'
+USUARIO_ID = 248608
+ZIP_MB = 350
+MOODLE_URL = 'moodle.uclv.edu.cu'
+OWNER = [LeonelMG]
+
+# CUENTA DE MEGA
+GMAIL_MEGA = 'leonelmolinaglez@gmail.com'
+PASS_MEGA = 'LeonelMG2003'
